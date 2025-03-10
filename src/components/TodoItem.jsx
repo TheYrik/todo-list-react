06799db17f8d01ada6b3e501a7fc8ec0d3c36fa2
Yrik <uragucol@gmail.com>
2025-03-10 13:56:34 +0200

    first commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { IoTrashBinOutline } from 'react-icons/io5'
+
+const TodoItem = ({ todo, toggleTask, removeTask }) => {
+	return (
+		<div className={todo.completed ? 'todo-row complete' : 'todo-row'}>		
+			<div className='todo-row-main' onClick={() => toggleTask(todo.id)}>
+			{todo.task}
+		</div>
+		<div className='iconsContainer'>
+				<IoTrashBinOutline onClick={() => removeTask(todo.id)} />g
+			</div>
+		</div>
+	)
+}
+
+export default TodoItem;
